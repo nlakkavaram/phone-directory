e28d37853237c9e8a08d9f6c843bfa4bef7bd9f0
nlakakvaram <nlakkavaram@netscoretech.com>
2022-09-11 20:52:38 +0530

    Created Skeleton of Home Page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-
 function App() {
   return (
     <div>
+      {/* <div className="header">
       Phone Directory
+      </div>
+      <button>Add</button>
+      <div>
+        <span>Name</span><br/>
+        <span>Phone</span>
+      </div> */}
+      <label htmlFor="name">Name : </label>
+      <input type="text" id="name" placeholder="Type Here" defaultValue="Jyothi" />
     </div>
   );
 }
