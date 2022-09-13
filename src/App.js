906e9724951e4694b5296ed33dbb74d59866d8fa
nlakakvaram <nlakkavaram@netscoretech.com>
2022-09-13 17:13:35 +0530

    Created ‘Header’ Component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
+import Header from "./Header";
+
 function App() {
+  let x = 2;
+  let y = 3;
   return (
     <div>
-      {/* <div className="header">
-      Phone Directory
-      </div>
+      <Header />
       <button>Add</button>
       <div>
         <span>Name</span><br/>
         <span>Phone</span>
+      </div>
+      {/* <label htmlFor="name">Name : </label>
+      <input
+        type="text"
+        id="name"
+        placeholder="Type Here"
+        defaultValue={x + y}
+      />
+
+      <div>
+        <label htmlFor="username">Username: </label> <br />
+        <input id="username" type="text" />
+        <br />
+        <br />
+        <label htmlFor="password">Password: </label>
+        <br />
+        <input id="password" type="password" />
+        <br />
+        <br />
       </div> */}
-      <label htmlFor="name">Name : </label>
-      <input type="text" id="name" placeholder="Type Here" defaultValue="Jyothi" />
     </div>
   );
 }
